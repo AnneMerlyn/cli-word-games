a5ea8f75fce414c4cefc805d4bc06655188e5ee4
Anne Martinez <martinezannemerlyn@gmail.com>
2025-01-14 18:27:34 +0100

    updated

--- rps-battle.js
+++ rps-battle.js
@@ -1,35 +1,32 @@
 const args = process.argv;
 const userInput = args.slice(2);
 
+const validInputs = ["rock", "paper", "scissor"];
+
 const checkUserInput = (userInput) => {
-    if (!userInput || userInput.length !== 1) {
+    if (!userInput || userInput.length < 1) {
+        console.log("Did you forget to choose? rock, paper, or scissors?");
+        process.exit(0);
+    } else if (userInput.length > 1) {
+        console.log("Hey, that's invalid! Way too much input...");
+        process.exit(0);
+    }
+
+    const userInputLower = userInput[0].toLowerCase();
+    if (!validInputs.includes(userInputLower)) {
         console.log(
             "Hold up! Only rock, paper, or scissors are allowed—no wildcards in this game!"
         );
         process.exit(0);
     }
-    const userInputLower = userInput[0].toLowerCase();
-    switch (userInputLower) {
-        case "rock":
-            return userInputLower;
-        case "paper":
-            return userInputLower;
-        case "scissor":
-            return userInputLower;
-        default:
-            console.log(
-                "Hold up! Only rock, paper, or scissors are allowed—no wildcards in this game!"
-            );
-            process.exit(0);
-    }
+    return userInputLower;
 };
 const validUserInput = checkUserInput(userInput);
 //console.log(validUserInput);
 
 const getComputerInput = () => {
-    const computerInputArray = ["rock", "paper", "scissor"];
-    const randomIndex = Math.floor(Math.random() * computerInputArray.length);
-    return computerInputArray[randomIndex];
+    const randomIndex = Math.floor(Math.random() * validInputs.length);
+    return validInputs[randomIndex];
 };
 
 const computerInput = getComputerInput();
